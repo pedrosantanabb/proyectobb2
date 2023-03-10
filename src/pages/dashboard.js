b67176286d76ecab784571cefedaeec70659687e
pedro.santana <pedro.santana@bitboxonline.com>
2023-03-10 15:45:20 +0000

    Add dashboard

---
+++ src/pages/dashboard.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from '@/styles/Home.module.css'
+
+export default function Dashboard(){
+    return(
+        <>
+            <Head>
+                <title>Dashboard</title>
+            </Head>
+            <h1>Dahsboard</h1>
+        </>
+    );
+}
